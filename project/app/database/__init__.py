7c6323cee16150a688cb6e5d9acb07909efc7e33
Tim Kreuzer <t.kreuzer@fz-juelich.de>
2023-12-01 19:52:16 +0100

    add sqlalchemy engine kwargs for a more stable connection

--- project/app/database/__init__.py
+++ project/app/database/__init__.py
@@ -13,11 +13,14 @@ SQL_PASSWORD = os.getenv("SQL_PASSWORD")
 SQL_PORT = os.getenv("SQL_PORT", "5432")
 SQL_USER = os.getenv("SQL_USER")
 db_url = ""
-engine_kwargs = {}
+
+# recycle – If set to a value other than -1, number of seconds between connection recycling, which means upon checkout, if this timeout is surpassed the connection will be closed and replaced with a newly opened connection. Defaults to -1.
+# pre_ping - if True, the pool will emit a “ping” (typically “SELECT 1”, but is dialect-specific) on the connection upon checkout, to test if the connection is alive or not. If not, the connection is transparently re-connected and upon success, all other pooled connections established prior to that timestamp are invalidated. Requires that a dialect is passed as well to interpret the disconnection error.
+engine_kwargs = {"pool_recycle": 300, "pool_pre_ping": True}
 
 if SQL_TYPE in ["sqlite", "sqlite+pysqlite"]:
     db_url = f"{SQL_TYPE}://{SQL_DATABASE_URL}"
-    engine_kwargs = {"connect_args": {"check_same_thread": False}}
+    engine_kwargs.update({"connect_args": {"check_same_thread": False}})
 elif SQL_TYPE == "postgresql":
     db_url = (
         f"{SQL_TYPE}://{SQL_USER}:{SQL_PASSWORD}@{SQL_HOST}:{SQL_PORT}/{SQL_DATABASE}"
