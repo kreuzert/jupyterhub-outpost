7fe73bb4091fc3fed986c762632904f21dd09e0e
Tim Kreuzer <t.kreuzer@fz-juelich.de>
2025-04-14 11:29:03 +0200

    update suppress ping filter

--- project/app/minimal_config.py
+++ project/app/minimal_config.py
@@ -6,21 +6,10 @@ from kubernetes import client
 from kubernetes import config
 from kubespawner import KubeSpawner
 
-logged_logger_name = os.environ.get("LOGGER_NAME", "Outpost")
-c.JupyterHubOutpost.log_format = f"%(color)s[%(levelname)1.1s %(asctime)s.%(msecs).03d {logged_logger_name} %(name)s %(module)s:%(lineno)d]%(end_color)s %(message)s"
 
-# Suppress /ping loggings, created by k8s livenessprobe
-uvicorn_access = logging.getLogger("uvicorn.access")
-
-
-class UvicornFilter(logging.Filter):
-    def filter(self, record):
-        try:
-            if "/ping" in record.args:
-                return False
-        except:
-            pass
-        return True
+class HealthCheckFilter(logging.Filter):
+    def filter(self, record: logging.LogRecord) -> bool:
+        return record.getMessage().find("/ping") == -1
 
 
 class EventsCheckFilter(logging.Filter):
@@ -30,9 +19,10 @@ class EventsCheckFilter(logging.Filter):
         return True
 
 
-uvicorn_access.addFilter(UvicornFilter())
-
-logging.getLogger(logged_logger_name).addFilter(EventsCheckFilter())
+logger_name = os.environ.get("LOGGER_NAME", "Outpost")
+c.JupyterHubOutpost.log_format = f"%(color)s[%(levelname)1.1s %(asctime)s.%(msecs).03d {logger_name} %(name)s %(module)s:%(lineno)d]%(end_color)s %(message)s"
+logging.getLogger("uvicorn.access").addFilter(HealthCheckFilter())
+logging.getLogger(logger_name).addFilter(EventsCheckFilter())
 
 c.JupyterHubOutpost.spawner_class = KubeSpawner
 
